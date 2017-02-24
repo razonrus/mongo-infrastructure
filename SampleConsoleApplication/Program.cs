43ba461338989bb1eaac39848f064bf40268810d
razonrus <iruslansafin@gmail.com>
2017-02-24 23:17:06 +0500

    cleanup

--- SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Infrastructure.Extensions;
-using MongoDB.Driver.Builders;
 using MongoDB.Driver.Linq;
 using SampleConsoleApplication.Database;
 using SampleConsoleApplication.Database.Domain;
